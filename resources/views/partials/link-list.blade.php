0c17edd1c180e88d8e71313f9677383235c03c41
Manoplo <manueluismartin@gmail.com>
2022-01-28 17:41:10 +0100

    A48 done

--- resources/views/partials/link-list.blade.php
+++ resources/views/partials/link-list.blade.php
@@ -18,7 +18,14 @@
     <a href="{{$link->link}}" target="_blank">
         {{$link->title}}
     </a>
+
+
+
+
+
     <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+    <small id="votes" class="votes">{{$link->users()->count()}} Votes</small>
+    <hr>
 
 </li>
 @endforeach
