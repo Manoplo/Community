ce177052809e058903bc8f6d35be2998711d58e1
Manoplo <manueluismartin@gmail.com>
2022-02-07 17:24:26 +0100

    A52 done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:link')->everyMinute()->withoutOverlapping();
+        $schedule->command('send:link')->dailyAt('08:00')->withoutOverlapping();
     }
 
     /**
